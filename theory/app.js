f88e41103434ce06e8f3ec6564b785b118aee26b
Anjana Devi <anjanadevi555551@gmail.com>
2026-06-15 20:21:03 +0530

    01-folder done

---
+++ theory/app.js
@@ -0,0 +1,10 @@
+const a = 20;
+
+const arr = [10,20,30,40,50]
+
+export {a};
+export default arr;
+/*
+1. Named
+2. Default : only one per file and nameless
+*/ 
---
+++ theory/script.js
@@ -0,0 +1,22 @@
+import {a} from "./app.js"
+import array from "./app.js"
+
+console.log(a);
+console.log(array);
+console.log(a);
+
+/* Creating element with JS
+var h1 = document.createElement('h1');
+
+h1.innerHTML = "Hello from JS";
+
+document.body.appendChild(h1);
+*/
+
+/*
+library = gives a feature to a website(one)
+GSAP(moving animation), Lenis(scrolling), ReactJS(UI)
+
+framework = in-built features
+NextJS, Angular
+*/
